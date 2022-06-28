43e9ddbfd8ca9078c2b6950459ba7d3927bff754
tsear <98225311+tsear@users.noreply.github.com>
2022-06-28 15:16:09 -0400

    Add files via upload

---
+++ The_Team.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+# creates sidebar title & page 3 title
+st.markdown("# The Team")
+st.sidebar.markdown("# The Team")
+
+# synopsis
+st.subheader('The team behind this project hopes to break into the fintech development job market. This application was built as the Project 2 for the Columbia BC submission.')
+
+
+
+
+
+st.write('- Donte Thrasher')
+st.write('- Philip DeNormandie')
+st.write('- Vikrant Sangar')
+st.write('- Grigory Timofeev')
+st.write('- Tyler Sear')
